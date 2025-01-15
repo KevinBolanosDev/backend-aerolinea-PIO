7af93d1ad55560889e5cedf8f9fd6fe17f104dee
KevinBolanosDev <im.kevinbolanos.dev@gmail.com>
2025-01-15 09:37:41 -0500

    new configuration

--- index.js
+++ index.js
@@ -18,11 +18,24 @@ app.use(
         callback(new Error('CORS not allowed for this origin'));
       }
     },
+    credentials: true,
   })
 );
 
 app.use(express.json());
 app.use(cookieParser());
+
+app.use(
+    session({
+      secret: SESSION_SECRET, // Cambia esto por una clave secreta segura
+      resave: false,
+      saveUninitialized: true,
+      cookie: {
+        secure: true, // true si estás usando HTTPS
+        sameSite: 'none', // Requerido para solicitudes cross-origin
+      },
+    })
+  );
 
 // Estás indicando que todas las rutas definidas en el archivo routes/index.js estarán disponibles bajo el prefijo '/api'
 app.use('/api', routes);
