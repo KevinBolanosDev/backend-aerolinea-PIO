e0c2baca8dd5bb9a480c5ab11ff311a16b9b2891
KevinBolanosDev <im.kevinbolanos.dev@gmail.com>
2025-01-15 09:00:01 -0500

    configuration

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ import routes from './src/routes/index.js';
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://dashboard-aerolinea.netlify.app/',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }))
 
